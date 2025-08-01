66b69d0c1ace8d97dbcdba0e7ee0c26119fa667f
kamrankhan0001 <kamranbgp15@gmail.com>
2025-08-01 10:48:37 +0530

    ch2

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -17,7 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const database = getDatabase(app);
+//export const database = getDatabase(app);
 
 // Get Firebase Auth instance
 const auth = getAuth(app);
